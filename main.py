e0fcc8f13ff410b1814565a9f69af777e4d382eb
wespatrocinio <wespatrocinio@gmail.com>
2020-05-26 10:26:50 -0300

    reestructuring some files

--- main.py
+++ main.py
@@ -1,35 +1,7 @@
-import autosklearn.classification
-import sklearn.model_selection
-import sklearn.metrics
+from automl_pipeline import config, features, data
 
-from automl_pipeline import config, persistence, features
+df = data.load_lyrics()
 
 pipeline_settings = config.load_pipeline()
 
 
-X, y = features.get_example_features()
-print('Train/test splitting')
-X_train, X_test, y_train, y_test = \
-        sklearn.model_selection.train_test_split(X, y, random_state=1)
-
-print('Loading sklearn classifier')
-automl = autosklearn.classification.AutoSklearnClassifier(
-    **pipeline_settings.get('pipeline')
-)
-
-print('Fitting automl')
-print(pipeline_settings)
-automl.fit(X_train, y_train)
-
-#This call to fit_ensemble uses all models trained in the previous call
-#to fit to build an ensemble which can be used with automl.predict()
-# automl.fit_ensemble(y_train, ensemble_size=50)
-
-print('Predicting')
-y_hat = automl.predict(X_test)
-
-print(automl.show_models())
-print(automl.sprint_statistics())
-print("Accuracy score", sklearn.metrics.accuracy_score(y_test, y_hat))
-
-persistence.save_model(automl, 'model/automl.joblib')
